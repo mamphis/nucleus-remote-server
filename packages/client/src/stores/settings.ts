82eed049313d47899ade2bf66e21d4e448432e0e
mamphis <mattis.wacker@pcsmw.de>
2023-08-26 19:11:29 +0200

    feat: change url dependent from environment

--- packages/client/src/stores/settings.ts
+++ packages/client/src/stores/settings.ts
@@ -1,6 +1,11 @@
 import { defineStore } from 'pinia';
 
 export const settingsStore = defineStore('settings', () => {
-  const baseApiUrl = 'http://srv-home1.home.int:3002/api/v1/';
-  return { baseApiUrl }
+  if (import.meta.env.DEV) {
+    const baseApiUrl = 'http://srv-home1.home.int:3002/api/v1/';
+    return { baseApiUrl }
+  } else {
+    const baseApiUrl = 'https://nrs.backend.pcsmw.de/api/v1/';
+    return { baseApiUrl }
+  }
 })
