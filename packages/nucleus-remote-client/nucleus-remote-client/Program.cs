340d8f27aba0201370015e325109632ffc0b9f08
Mattis <mattis.wacker@pcsmw.de>
2023-08-28 22:50:04 +0200

    feat(client): added tasks `DownloadFile` and `ExecuteFile`

--- packages/nucleus-remote-client/nucleus-remote-client/Program.cs
+++ packages/nucleus-remote-client/nucleus-remote-client/Program.cs
@@ -6,23 +6,7 @@ builder.Services.Configure<HostSettings>(
     builder.Configuration.GetSection("HostSettings")
 );
 
-var ServiceName = "nucleus-remote-client";
-builder.Services.AddWindowsService(options =>
-{
-    options.ServiceName = ServiceName;
-});
-
 builder.Services.AddHostedService<Worker>();
-
-if (args.Contains("install"))
-{
-    Installer.GrantUserToLogonAsService();
-    Installer.Install(ServiceName);
-}
-if (args.Contains("uninstall"))
-{
-    Installer.Uninstall(ServiceName);
-}
 
 var host = builder.Build();
 host.Run();
---
+++ packages/nucleus-remote-client/nucleus-remote-client/Tasks/ExecuteFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nucleus_remote_client.Tasks
+{
+    internal class ExecuteFile : ITask
+    {
+        public string File { get; set; }
+        public string? Arguments { get; set; }
+        public bool HideWindow { get; set; }
+        public bool StartIfProcessIsRunning { get; set; }
+
+        private static Dictionary<string, int> StartedProcesses = new Dictionary<string, int>();
+
+        public async Task Run(HostSettings hostSettings)
+        {
+            var path = PathHelper.GetPath(File);
+
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            if (StartedProcesses.ContainsKey(path) && !this.StartIfProcessIsRunning)
+            {
+                var existingProc = Process.GetProcesses().FirstOrDefault(p => p.Id == StartedProcesses[path]);
+                if (existingProc != null && !existingProc.HasExited)
+                {
+                    return;
+                }
+
+                StartedProcesses.Remove(path);
+            }
+
+            var psi = new ProcessStartInfo(path, this.Arguments ?? "");
+            psi.CreateNoWindow = this.HideWindow;
+
+            var proc = Process.Start(psi);
+            if (proc == null)
+            {
+                throw new Exception("The process cannot be started.");
+            }
+
+            ExecuteFile.StartedProcesses.Add(path, proc.Id);
+        }
+    }
+}
