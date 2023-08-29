b807c6597f91c8970f0cfd9eb90edb0d5ccf0d9d
mamphis <mattis.wacker@pcsmw.de>
2023-08-29 11:59:18 +0200

    feat: Implement Prune Job Fixes #15

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,17 +7,26 @@ import { isProduction } from './lib/util';
 import { needSeed, seed } from "./seed";
 Logger.info('Production Environment:', isProduction());
 
+import { PrismaClientInitializationError } from '@prisma/client/runtime/library';
 import mailer from './lib/mailer';
+import init from './lib/tasks';
 import { Server } from './server/server';
 
 const port = Number(process.env.PORT);
 
 const start = async () => {
-    const db = new PrismaClient();
-
-    if (await needSeed(db)) {
-        Logger.info('Empty database found. Seeding database.');
-        await seed(db);
+    try {
+        const db = new PrismaClient();
+        await db.$connect();
+        if (await needSeed(db)) {
+            Logger.info('Empty database found. Seeding database.');
+            await seed(db);
+        }
+    } catch (e: unknown) {
+        if (e instanceof PrismaClientInitializationError) {
+            Logger.fatal(e.message);
+            process.exit(1);
+        }
     }
 
     await mailer.init();
@@ -28,9 +37,11 @@ const start = async () => {
     await server.start().catch(e => {
         if (e.code === 'EADDRINUSE') {
             Logger.fatal('Cannot start server because the port is already in use: ' + port);
-            process.exit();
+            process.exit(1);
         }
     });
+
+    init();
 }
 
 start();
---
+++ packages/backend/src/lib/tasks.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client"
+import { Logger } from "./logger";
+import { schedule } from 'node-cron';
+
+const removeDanglingTasks = async () => {
+    const db = new PrismaClient();
+    const tasksBefore = await db.task.count();
+
+    await db.task.deleteMany({
+        where: {
+            configuration: {
+                is: null,
+            }
+        }
+    });
+
+    const tasksAfter = await db.task.count();
+    Logger.info(`Task "removeDanglingTasks" has run. Removed ${tasksBefore - tasksAfter} tasks.`);
+}
+
+const init = () => {
+    // Run every second day at 03:00 in the morning
+    schedule("0 3 * * */2", removeDanglingTasks, { name: 'removeDanglingTasks', runOnInit: true });
+}
+
+export default init;
