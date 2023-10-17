017b78b0dc9b96e5487cc02e4dad36fde2e8769a
mamphis <mattis.wacker@pcsmw.de>
2023-10-17 22:55:42 +0200

    feature(frontend): implement call to get secure configuration

--- packages/client/src/views/Clients/ClientsView.vue
+++ packages/client/src/views/Clients/ClientsView.vue
@@ -7,12 +7,30 @@ import userStore from '@/stores/user';
 import type { ApiClient } from '@/types/client';
 import type { ApiTenant } from '@/types/tenant';
 
-const clients = await request.$get<ApiClient[]>('clients');
+const clientResponse = await request.$get<ApiClient[]>('clients');
+const clients = clientResponse.assertNotError().toRef();
 const { baseApiUrl } = settingsStore();
 const { user } = userStore();
 const tenantResponse = await request.$get<Pick<ApiTenant, 'maxClients'>>(`tenants/${user?.tenantId}`);
 const tenant = tenantResponse.assertNotError();
 const fileUrl = new URL(`/system/update/file`, baseApiUrl).href;
+
+const downloadConfigurationFile = async () => {
+    const response = await request.$raw('GET', 'clients/configuration');
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'appsettings.json';
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
+
+    const clientResponse = await request.$get<ApiClient[]>('clients');
+    clients.value = clientResponse.assertNotError();
+};
+
 </script>
 <template>
     <div class="columns is-flex-grow-1 is-multiline is-align-content-flex-start is-h-100">
@@ -24,7 +42,7 @@ const fileUrl = new URL(`/system/update/file`, baseApiUrl).href;
             <div class="column is-flex is-justify-content-end">
                 <div class="buttons is-grouped">
                     <a class="button" v-if="hasPermission(user, 'create:client')"
-                        :href="baseApiUrl + `clients/configuration/${user?.tenantId}`">
+                        @click.preventDefault="downloadConfigurationFile()">
                         {{ $t('clients.newClient') }}</a>
                     <a class="button" v-if="hasPermission(user, 'create:client')" :href="fileUrl">
                         {{ $t('clients.downloadClient') }}</a>
