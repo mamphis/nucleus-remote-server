c2dff74c689cf88e3bd1ec886eab77e447601d45
mamphis <mattis.wacker@pcsmw.de>
2023-10-23 22:26:23 +0200

    fix: return correct queries

--- packages/backend/src/server/routes/v1/admin.ts
+++ packages/backend/src/server/routes/v1/admin.ts
@@ -39,39 +39,25 @@ export default function (db: PrismaClient) {
     });
 
     router.get('/sqlHistogram', auth('special:admin'), async (req, res, next) => {
-        const data = await db.requestMetrics.findMany({
-            select: {
-                avgDuration: true,
-                maxDuration: true,
-                hitCount: true,
-                bucketTime: true,
-                statusCodes: {
-                    select: {
-                        statusCode: true,
-                        hitCount: true,
-                    }
-                }
-            },
+        const histogram = await db.queryMetrics.groupBy({
+            by: 'bucketTime',
+            _sum: { hitCount: true },
+            _avg: { avgDuration: true },
+            _max: { maxDuration: true },
             where: {
                 bucketTime: {
-                    gt: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
+                    gte: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
                 }
             }
-        });   
-
-        const histogram = data.map((metric) => ({
-            bucketTime: metric.bucketTime,
-            hitCount: metric.hitCount,
-            avgDuration: metric.avgDuration,
-            maxDuration: metric.maxDuration,
-            statusCodes: metric.statusCodes.map((statusCode) => ({
-                statusCode: statusCode.statusCode,
-                hitCount: statusCode.hitCount,
-            })),
-        }));
+        });
 
         res.json({
-            histogram,
+            histogram: histogram.map((metric) => ({
+                bucketTime: metric.bucketTime,
+                hitCount: metric._sum.hitCount,
+                avgDuration: metric._avg.avgDuration,
+                maxDuration: metric._max.maxDuration,
+            })),
         })
     });
 
@@ -131,11 +117,19 @@ export default function (db: PrismaClient) {
     });
 
     router.get('/requestHistogram', auth('special:admin'), async (req, res, next) => {
-        const histogram = await db.requestMetrics.groupBy({
-            by: 'bucketTime',
-            _sum: { hitCount: true },
-            _avg: { avgDuration: true },
-            _max: { maxDuration: true },
+        const data = await db.requestMetrics.findMany({
+            select: {
+                avgDuration: true,
+                maxDuration: true,
+                hitCount: true,
+                bucketTime: true,
+                statusCodes: {
+                    select: {
+                        statusCode: true,
+                        hitCount: true,
+                    }
+                }
+            },
             where: {
                 bucketTime: {
                     gt: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
@@ -143,15 +137,19 @@ export default function (db: PrismaClient) {
             }
         });
 
-        
+        const histogram = data.map((metric) => ({
+            bucketTime: metric.bucketTime,
+            hitCount: metric.hitCount,
+            avgDuration: metric.avgDuration,
+            maxDuration: metric.maxDuration,
+            statusCodes: metric.statusCodes.map((statusCode) => ({
+                statusCode: statusCode.statusCode,
+                hitCount: statusCode.hitCount,
+            })),
+        }));
 
         res.json({
-            histogram: histogram.map((metric) => ({
-                bucketTime: metric.bucketTime,
-                hitCount: metric._sum.hitCount,
-                avgDuration: metric._avg.avgDuration,
-                maxDuration: metric._max.maxDuration,
-            })),
+            histogram,
         })
     });
 
